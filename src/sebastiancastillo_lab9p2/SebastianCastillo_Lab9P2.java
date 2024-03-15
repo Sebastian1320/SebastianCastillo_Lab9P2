7b46bdadda32776671d88f2fc91e1f57c5a8cf11
Sebastian <sebastian13200000@gmail.com>
2024-03-15 13:13:24 -0600

    Primer Commit

---
+++ src/sebastiancastillo_lab9p2/SebastianCastillo_Lab9P2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sebastiancastillo_lab9p2;
+
+/**
+ *
+ * @author Asus
+ */
+public class SebastianCastillo_Lab9P2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
